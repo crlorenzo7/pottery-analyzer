198169925217fc7854f990164ecddc02d1782388
Moviefinder-admin <crlorenzo777@gmail.com>
2019-06-12 15:17:33 +0200

    test

--- pottery/settings.py
+++ pottery/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9i71^=^anivjxq2ca(fw4fpm7_bc*i(p(*c^&zfzb9$bh2icy0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
